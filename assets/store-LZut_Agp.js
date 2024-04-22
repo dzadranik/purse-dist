4ad448c3022169bbf13dd333582ea525ffe28eca
Victoria Korotkova <korotkovava@yandex.ru>
2024-04-22 12:16:06 +0300

    deploy

---
+++ assets/store-LZut_Agp.js
@@ -0,0 +1,1 @@
+import{k as r}from"./index-Da3riP-4.js";const a=r({currencyPairValue:"BTCUSDT"});export{a as s};
--- assets/store-u6yp0mFh.js
+++
@@ -1,1 +0,0 @@
-import{k as r}from"./index-BrON4RyW.js";const a=r({currencyPairValue:"BTCUSDT"});export{a as s};
